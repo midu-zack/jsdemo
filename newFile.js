612c7f2dd1593a643496f8da8ded8b4fe3f41cde
midlaj <miduzack001@gmail.com>
2023-10-25 20:56:16 +0530

    zack

---
+++ newFile.js
@@ -0,0 +1,33 @@
+// anonymens function
+
+let fun1 = function (){
+    console.log("heyy all");
+}
+
+fun1();
+
+
+// arrow function 
+
+let fun2 = () => console.log("zackk all");
+
+
+fun2();
+
+// find sum in function
+
+let sum = (x,y) => console.log(x+y);
+
+sum(80,20);
+
+
+// if condition in arrow function
+
+let number = (num) =>{ if (num<0){
+    console.log("Number is negtive");
+}else{
+    console.log("number is positive");
+}
+}
+
+number(-23);
