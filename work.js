c3f27f8c513bf3596ff898088ff22225b65367a5
midlaj <miduzack001@gmail.com>
2023-10-27 20:59:43 +0530

    do

---
+++ work.js
@@ -0,0 +1,122 @@
+// debugging
+// console.log(x);
+// console.log(getname);
+
+// var x = 20;
+
+
+// function getname(){
+//     console.log("Hello zack");
+// }
+
+// getname();
+
+
+// var x=19;
+// a();
+// b();
+// console.log(x);
+
+
+// function a(){
+//     var x=10;
+//     console.log(x);
+// }
+// function b(){
+//     var x=38;
+//     console.log(x);
+// }
+
+
+// var a=10;
+// console.log(a);
+
+ 
+// var a = 29;
+
+
+// if(a===undefined){
+//     console.log("a is undifined");
+// }else{
+//     console.log("a is not undfineed");
+// }
+
+
+// var a;
+// console.log(a);
+// a=19;
+// console.log(a);
+// a="bueryfull";
+// console.log(a) ;
+
+
+
+
+// hoisting
+
+// call();
+// console.log(a);
+// calling();
+
+
+// var a=12;
+// function call(){
+//     console.log("True");
+// }
+
+// call();
+
+//  calling = () =>{
+//     console.log("false");
+
+//  }
+
+
+
+// lexical scope
+// function a (){
+//     x ();
+//     function x (){
+//         console.log(b);
+//     }
+
+// }
+// var b = 23;
+
+// a();
+
+
+
+// let a=10;
+// console.log(a);
+// var b=100;
+// b=23;
+// console.log(b);
+
+// const c=42;
+// c=42;
+// console.log(c);
+
+// let m=873;
+// let m=873;
+// console.log(m);
+
+
+
+
+
+// let and const block scoped
+ 
+if(true) {
+    // compound statement
+
+    let a = 10;
+    console.log(a);
+};
+
+
+
+
+
+
+
